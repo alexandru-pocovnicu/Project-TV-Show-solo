38e4911f4a913370c9a00a9570872bf3ed3a3f9c
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-05-05 14:28:47 +0100

    extract details for one episode

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 //You can edit ALL of the code here
+const allEpisodes = getAllEpisodes();
+
 function setup() {
-  const allEpisodes = getAllEpisodes();
+  extractEpisodeDetails(allEpisodes);
   makePageForEpisodes(allEpisodes);
+  console.log(extractEpisodeDetails(allEpisodes));
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+}
+
+function extractEpisodeDetails(allEpisodes) {
+  let episodeName = document.createElement("h2");
+  let episodeCode = document.createElement("h3");
+  let episodeImage = document.createElement("img");
+  let episodeSummary = document.createElement("p");
+  let episodeCard = document.createElement("section");
+  let seasonNumber;
+  let episodeNumber;
+  for (let episode of allEpisodes) {
+    episodeName.innerText = episode.name;
+    seasonNumber = episode.season.toString();
+    episodeNumber = episode.number.toString();
+    episodeSummary.innerText = episode.summary;
+    episodeImage.src = episode.image.medium;
+    episodeImage.alt = episode.name;
+    episodeImage.width = "300";
+    episodeCode.innerText = `S${seasonNumber.padStart(2, "0")}E${episodeNumber.padStart(2, "0")}`;
+    episodeCard.append(episodeName, episodeCode, episodeImage, episodeSummary);
+  }
+  
+  return episodeCard;
 }
 
 window.onload = setup;
